398dbb2580a7ff8f678d593f7127ed116840fee5
Krishna <Krishnagotur17@gmail.com>
2023-11-04 12:02:17 +0530

    Flavoured-Tea-EP-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const Heading = () =>{
+    return <h1>Flavoured</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Heading/>);
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# This is Flavoured Tea Shop
+where you can get your desired Flavour of Tea.
+
+# Flavoured ☕
